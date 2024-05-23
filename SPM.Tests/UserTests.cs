69348765f6a00e33bbd860bc1e6ce942ee5acff9
Joseph Clifford <92230691+jclifford4@users.noreply.github.com>
2024-05-22 22:00:07 -0700

    Begin user tests

---
+++ SPM.Tests/UserTests.cs
@@ -0,0 +1,14 @@
+using Xunit;
+using SPM;
+
+namespace SPM.Tests
+{
+    public class UserTests
+    {
+        [Fact]
+        public string Create_User_ReturnUsernameAsString()
+        {
+            return "";
+        }
+    }
+}
